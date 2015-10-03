67e0aa96a5e3c38940e2827842d754d942a8deb9
Gregory Higley <code@revolucent.net>
2015-10-03 05:50:22 -0400

    Option and Apply anchors, etc.

--- Prosumma/RangeExtensions.swift
+++ Prosumma/RangeExtensions.swift
@@ -16,3 +16,17 @@ public func +<E: IntegerArithmeticType>(lhs: Range<E>, rhs: Range<E>) -> Range<E
     let endIndex = startIndex.advancedBy(distance - 1)
     return startIndex..<endIndex
 }
+
+extension NSRange {
+
+    public init<I: Indexable where I.Index.Distance == Int>(_ indexable: I, range: Range<I.Index>) {
+        location = indexable.startIndex.distanceTo(range.startIndex)
+        length = range.startIndex.distanceTo(range.endIndex)
+    }
+    
+    public init(_ string: String, range: Range<String.Index>) {
+        location = string.startIndex.distanceTo(range.startIndex)
+        length = range.startIndex.distanceTo(range.endIndex)
+    }
+    
+}
